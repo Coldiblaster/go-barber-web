3187f0e51b11d70f379ed0b64416d80775b56699
Coldiblaster <vbastazin@gmail.com>
2020-10-07 14:30:29 -0300

    Implementando Login pelo Contexto

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,18 +1,29 @@
 import React, { createContext, useCallback } from 'react';
+import api from '../services/apiClient';
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
 
 interface AuthContextData {
   name: string;
+  signIn(credentials: SignInCredentials): Promise<void>;
 }
 
-const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+export const AuthContext = createContext<AuthContextData>(
+  {} as AuthContextData,
+);
 
 export const AuthProvider: React.FC = ({ children }) => {
-  const singIn = useCallback(() => {
-    console.log('singIn');
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', email, password);
+
+    console.log(response.data);
   }, []);
 
   return (
-    <AuthContext.Provider value={{ name: 'Diego' }}>
+    <AuthContext.Provider value={{ name: 'Diego', signIn }}>
       {children}
     </AuthContext.Provider>
   );
